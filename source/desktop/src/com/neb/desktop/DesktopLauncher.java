728c4762e17ccb3d08befb411f01fbf105837148
Nathan Baney <nathanebaney@gmail.com>
2019-03-19 13:20:54 -0700

    added basic libgdx stuff

---
+++ source/desktop/src/com/neb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.neb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.neb.ASLRip;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ASLRip(), config);
+	}
+}
